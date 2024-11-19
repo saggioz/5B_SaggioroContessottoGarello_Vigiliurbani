6ac1c5d0e3c784c8db01d917ccb86b343cf2d429
Fabio Contessotto <contessottofabio@itis-molinari.eu>
2024-11-19 12:27:08 +0100

    fixed table

--- tabella.js
+++ tabella.js
@@ -1,13 +1,18 @@
 import { SETTABELLA, GETTABELLA } from './progetto.js';
 const createTable = (parentElement) => {
     let data = [];
+    let headers = [];
+    let originale = [];
     return {
         build: (dataInput) => {
+            headers = dataInput[0];
             data = dataInput;
+            originale = dataInput;
         },
         render: () => {
             let htmlTable = "<table class='table table-bordered'>";
-            htmlTable += "<thead><tr>" + data[0].map((col) => `<td>${col}</td>`).join("") + "</tr></thead>";
+            htmlTable += "<thead><tr>" + headers.map((header) => `<th>${header}</th>`).join("") + "</tr></thead>";
+            htmlTable += "<tr>" + data[0].map((col) => `<td>${col}</td>`).join("") + "</tr>";
             htmlTable += "<tbody>" + 
                 data.slice(1).map((row) => 
                     "<tr>" + row.map((col) => 
@@ -24,10 +29,23 @@ const createTable = (parentElement) => {
                 console.error("Errore durante il salvataggio della tabella nella cache:", err);
             });
         },
+        filter: function(cerca) {
+            if (cerca === "") {
+                data = originale;
+            } else {
+                data = originale.filter(row => 
+                    row[0].toLowerCase().includes(cerca.toLowerCase())
+                );
+            }
+            this.render();
+        },
         load: function () {
             GETTABELLA().then((cachedData) => {
-                    data = cachedData;
-                    this.render();                
+                originale = cachedData;
+                data = cachedData;
+                this.render();
+            }).catch((err) => {
+                console.error("Errore durante il caricamento dei dati dalla cache:", err);
             });
         },
     };
@@ -35,4 +53,11 @@ const createTable = (parentElement) => {
 
 const table = createTable(document.querySelector("#table"));
 table.build([["INDIRIZZO", "TARGHE COINVOLTE", "DATA", "ORA", "NUMERO FERITI", "NUMERO MORTI"]]);
+table.load();
+
+document.getElementById("bottoneRicerca").onclick = () => {
+    const cerca = document.getElementById("ricerca").value;
+    table.filter(cerca);
+};
+
 export { table };
