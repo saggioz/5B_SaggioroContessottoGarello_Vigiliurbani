6a0adc97282bfe28d5a4afb838eec285037a5c9f
Fabio <contessottofabio@itis-molinari.eu>
2024-11-23 21:14:59 +0100

    login

---
+++ login.js
@@ -0,0 +1,12 @@
+const TOKEN = "3819207b-2545-44f5-9bce-560b484b2f0f";
+
+const REGISTER_URL = "https://ws.cipiaceinfo.it/credential/register";
+const LOGIN_URL = "https://ws.cipiaceinfo.it/credential/login";
+
+const registerButton = document.getElementById("register-button");
+const loginButton = document.getElementById("login-button");
+const Private = document.getElementById("private-section");
+const registerUsername = document.getElementById("register-username");
+const registerPassword = document.getElementById("register-password");
+const loginUsername = document.getElementById("login-username");
+const loginPassword = document.getElementById("login-password");
